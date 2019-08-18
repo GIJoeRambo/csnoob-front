7b87499dc7f507c4cd9275ec2f7ba761bb4ff56a
GIJoeRambo <richardnzworking@gmail.com>
2019-08-19 00:03:27 +1200

    a

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from './hoc/Aux'
+
+const App = () => {
+  return (
+      <Aux>
+          <h1 className='text-danger'>Fuck You Richard</h1>
+      </Aux>
+  );
+}
+
+export default App;
