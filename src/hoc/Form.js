33ec2da07cf3aa17e16ea2c057733bdf9af45ce0
GIJoeRambo <richardnzworking@gmail.com>
2019-08-19 19:38:20 +1200

    a

---
+++ src/hoc/Form.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+
+const Form = (fileds) =>{
+    return WrappedForm =>{
+        return class extends Component{
+            state = {...fileds};
+
+            InputChangeHandler = (event) => {
+                console.log(event.target.value)
+                this.setState({
+                    [event.target.name]: event.target.value
+                })
+            }
+
+            SubmitHandler = (event, onSubmit) =>{
+                event.preventDefault();
+                onSubmit();
+            }
+
+
+
+
+            render(){
+                return (
+                    <WrappedForm
+                        {...this.props}
+                        onChange={this.InputChangeHandler.bind(this)}
+                        onSubmit={this.SubmitHandler.bind(this)}
+                        fields={{ ...this.state }}
+                    />
+                )
+            }
+
+        }
+    }
+};
+
+export default Form
+
+
+// const Data = ({onSubmit,onChange,fields}) =>{
+//     const handleSubmit = event =>{
+//         onSubmit(event,()=>{
+//             console.log(fields.age)
+//         })
+//     }
+//     return (
+//         <div>
+//             <input
+//                 value={fields.age}
+//                 name="age"
+//                 onChange={onChange}
+//             />
+//             <input
+//                 value={fields.name}
+//                 name="name"
+//                 onChange={onChange}
+//             />
+//             <button onClick={handleSubmit}>confirm</button>
+//         </div>
+//     )
+// }
+//
+// export default Form({name:"oliver",age:"10"})(Data)
