800320d5caca3de0d4b5832fdda5c3fe30d83c2b
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-22 12:08:51 +1200

    update

--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -1,45 +1,20 @@
 import React from "react";
 import DashboardLayout from "../../components/layout/dashboard_layout/dashboard_layout";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import UoA from "./uoa/uoa";
 import Aut from "./aut/aut";
 import Massey from "./massey/massey";
 
-class Dashboard extends React.Component {
-  state = {
-    oldUrl: this.props.location.pathname
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    if (props.location.pathname !== state.oldUrl) {
-      console.log(1);
-      setTimeout(() => {
-        window.location.reload();
-      }, 0);
-      // return {
-      //   oldUrl: props.location.pathname
-      // };
-    }
-    return null;
-  }
-
-  render() {
-    console.log(this.state);
-    return (
-      <DashboardLayout>
-        <Router>
-          <Switch>
-            <Route path={`${this.props.match.path}/uoa`} component={UoA} />
-            <Route path={`${this.props.match.path}/aut`} component={Aut} />
-            <Route
-              path={`${this.props.match.path}/massey`}
-              component={Massey}
-            />
-          </Switch>
-        </Router>
-      </DashboardLayout>
-    );
-  }
-}
+const Dashboard = props => {
+  return (
+    <DashboardLayout>
+      <Switch>
+        <Route path={`${props.match.path}/uoa`} component={UoA} />
+        <Route path={`${props.match.path}/aut`} component={Aut} />
+        <Route path={`${props.match.path}/massey`} component={Massey} />
+      </Switch>
+    </DashboardLayout>
+  );
+};
 
 export default Dashboard;
