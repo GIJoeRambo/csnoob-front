309e9b9e108523a22bcbb108b32e42303cf67ed9
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-27 23:48:27 +1200

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./containers/home/Home";
 import Dashboard from "./containers/dashboard/dashboard";
 import NotFound from "./containers/NotFound/NotFound";
 import MainLayout from "./components/layout/main_layout/main_layout";
+import CourseView from "./containers/course/CourseView";
 import "./App.css";
 import {
   BrowserRouter as Router,
@@ -20,6 +21,7 @@ const App = () => {
           <Route exact path="/404" component={NotFound} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/forum" component={Forum} />
+          <Route path="/course/:uniName/:courseCode" component={CourseView} />
           <Route path="/" exact component={Home} />
           <Redirect from={"*"} to={"/404"} />
         </Switch>
