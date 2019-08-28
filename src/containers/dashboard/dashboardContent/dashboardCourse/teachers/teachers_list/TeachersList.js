20b626b72519f412748ce22ead4cc609423306b9
oliver <oliverdeng1005@hotmail.com>
2019-08-29 00:14:32 +1200

    update

--- src/containers/dashboard/dashboardContent/dashboardCourse/teachers/teachers_list/TeachersList.js
+++ src/containers/dashboard/dashboardContent/dashboardCourse/teachers/teachers_list/TeachersList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 import './TeachersList.css';
 
--- src/containers/forum/ForumContent/ForumContent.js
+++ src/containers/forum/ForumContent/ForumContent.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react'
 import ForumContentList from "./forumContentList/forumContentList";
 import ForumContentItem from "./forumContentList/forumContentItem/forumContentItem";
 import service from '../../../service/http'
+import decoder from '../../../util/Decoder'
 class ForumContent extends Component{
     state = {
         data: []
@@ -27,7 +28,7 @@ class ForumContent extends Component{
                             key={index}
                             title={s.title}
                             postAuthor="Oliver Deng"
-                            when="Yesterday"
+                            when={decoder(s._id).toDateString()}
                             replyNum="1"
                             LastReplyName="Richard Wang"
                             LastReplyTime="2 hours ago"
