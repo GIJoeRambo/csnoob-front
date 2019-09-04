4c7e828a08dcbb3a8253de9c6a7953bbbf26bedd
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-09-04 16:02:19 +1200

    update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# 问题
+# todos
 
-1. 评论排序
+Justin todos:
+
+1. 重写sidebar
+2. header加fourm导航
 
 Oliver下一步要做:
+
 1. 设计Thread评论modal/paghe
 2. ThreadComment读取，显示出来进行分页
--- src/containers/forum/Thread/Thread.js
+++ src/containers/forum/Thread/Thread.js
@@ -1,109 +1,122 @@
-import React,{Component} from 'react'
-import {Helmet} from 'react-helmet'
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import ThreadHeader from "./ThreadHeader/ThreadHeader";
 import ThreadContent from "./ThreadContent/ThreadContent";
 import ThreadComment from "./ThreadComment/ThreadComment";
 import BackButton from "../backButton/backButton";
-import queryString from 'query-string'
-import {Redirect} from 'react-router-dom'
-import service from '../../../service/http'
+import queryString from "query-string";
+import { Redirect } from "react-router-dom";
+import service from "../../../service/http";
 import ThreadCommentTextPane from "./ThreadCommentTextPane/ThreadCommentTextPane";
-import decoder from '../../../util/Decoder'
+import decoder from "../../../util/Decoder";
 import { connect } from "react-redux";
-import moment from 'moment'
+import moment from "moment";
 
-class Thread extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            author: "",
-            content:"",
-            isRedirect: false,
-            comments:[]
-        }
+class Thread extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      author: "",
+      content: "",
+      isRedirect: false,
+      comments: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (nextProps.IsPosted !== this.props.IsPosted) {
+      this.getThreadCommentsHandler(
+        queryString.parse(this.props.location.search).id
+      );
     }
+  }
 
-    componentWillReceiveProps(nextProps, nextContext) {
-        if (nextProps.IsPosted !== this.props.IsPosted){
-            this.getThreadCommentsHandler(queryString.parse(this.props.location.search).id)
-        }
+  getThreadCommentsHandler = ThreadId => {
+    service.getThreadCommentByThreadId(
+      ThreadId,
+      res => {
+        this.setState({
+          comments: res.Data
+        });
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  };
+
+  componentDidMount = () => {
+    service.getThreadByThreadId(
+      queryString.parse(this.props.location.search).id,
+      res => {
+        this.setState({
+          title: res.Data.title,
+          author: res.Data.author,
+          content: res.Data.content
+        });
+      },
+      err => {
+        this.setState({
+          isRedirect: true
+        });
+      }
+    );
+    this.getThreadCommentsHandler(
+      queryString.parse(this.props.location.search).id
+    );
+  };
+
+  render() {
+    if (this.state.isRedirect) {
+      return <Redirect to="/forum" />;
     }
+    return (
+      <div className="col-large push-top">
+        <Helmet>
+          <style>{"body { background-color: #F6F8FF; }"}</style>
+        </Helmet>
+        <BackButton click={() => this.props.history.goBack()} />
+        <ThreadHeader
+          title={this.state.title}
+          author={this.state.author}
+          replies={this.state.comments.length}
+          PostDate={moment(
+            decoder(queryString.parse(this.props.location.search).id)
+          ).fromNow()}
+        />
+        <div className="post-list">
+          <ThreadContent content={this.state.content} />
+          <h2>Comments</h2>
+          {this.state.comments.map((s, index) => {
+            return (
+              <ThreadComment
+                floor={index + 1}
+                name={s.name}
+                key={index}
+                content={s.comment}
+                postDate={moment(decoder(s._id)).fromNow()}
+              />
+            );
+          })}
 
-    getThreadCommentsHandler = (ThreadId) =>{
-        service.getThreadCommentByThreadId(ThreadId,res=>{
-            this.setState({
-                comments:res.Data
-            })
-        },err=>{
-            console.log(err)
-        })
-    }
-
-    componentDidMount = ()=> {
-        service.getThreadByThreadId(queryString.parse(this.props.location.search).id,res=>{
-            this.setState({
-                title:res.Data.title,
-                author:res.Data.author,
-                content:res.Data.content
-            })
-        },err=>{
-            this.setState({
-                isRedirect:true
-            })
-        })
-        this.getThreadCommentsHandler(queryString.parse(this.props.location.search).id)
-    }
-
-    render() {
-        if (this.state.isRedirect){
-            return <Redirect to="/forum"/>
-        }
-        return (
-            <div className="col-large push-top">
-                <Helmet>
-                    <style>{'body { background-color: #F6F8FF; }'}</style>
-                </Helmet>
-                <BackButton
-                    click={()=>this.props.history.goBack()}
-                />
-                <ThreadHeader
-                    title={this.state.title}
-                    author={this.state.author}
-                    replies={this.state.comments.length}
-                    PostDate={moment(decoder(queryString.parse(this.props.location.search).id)).fromNow()}
-                />
-                <div className="post-list">
-                    <ThreadContent
-                        content={this.state.content}
-                    />
-                    <h2>Comments</h2>
-                    {this.state.comments.map((s,index)=>{
-                        return (
-                            <ThreadComment
-                                floor={index+1}
-                                name={s.name}
-                                key={index}
-                                content={s.comment}
-                                postDate={moment(decoder(s._id)).fromNow()}
-                            />
-                        )
-                    })}
-
-                    <div>
-                        <h2>Write the comment</h2>
-                        <ThreadCommentTextPane
-                            threadId={queryString.parse(this.props.location.search).id}
-                        />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          <div>
+            <h2>Write the comment</h2>
+            <ThreadCommentTextPane
+              threadId={queryString.parse(this.props.location.search).id}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => {
+  console.log(state);
+  return {
     IsPosted: state.ThreadCommentIsPosted
-});
+  };
+};
 
-export default connect(mapStateToProps)(Thread)
+export default connect(mapStateToProps)(Thread);
