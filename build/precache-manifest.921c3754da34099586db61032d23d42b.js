e08f6fb006bf473e267e58d075e82eaf34e07389
Oliver <oliverdeng1005@hotmail.com>
2020-03-09 14:42:49 +1300

    update

---
+++ build/precache-manifest.921c3754da34099586db61032d23d42b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc48e14fefac8f4083f75445b6a7ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9fd6452d29827dd8fb",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "197de3fc45abfa819f9b",
+    "url": "/static/css/main.9a9f0e18.chunk.css"
+  },
+  {
+    "revision": "1a9fd6452d29827dd8fb",
+    "url": "/static/js/2.d119b8fe.chunk.js"
+  },
+  {
+    "revision": "197de3fc45abfa819f9b",
+    "url": "/static/js/main.a3e12d05.chunk.js"
+  },
+  {
+    "revision": "5c016fca8f2956d1786e",
+    "url": "/static/js/runtime~main.d64bca8d.js"
+  },
+  {
+    "revision": "32cc46320d06ec0c60125c121dd6656e",
+    "url": "/static/media/Lego.32cc4632.png"
+  },
+  {
+    "revision": "e430101033efff9a294eaafecbac846a",
+    "url": "/static/media/WireBiting.e4301010.gif"
+  },
+  {
+    "revision": "052d2e8d6b5249854ac2d4714f3538a4",
+    "url": "/static/media/aut.052d2e8d.bin"
+  },
+  {
+    "revision": "bddb32dc454fd639928a40f0d6a9d0e2",
+    "url": "/static/media/coding3.bddb32dc.jpg"
+  },
+  {
+    "revision": "1aae2fd33e28a4724b20a8e54bb7ca23",
+    "url": "/static/media/lincoln.1aae2fd3.bin"
+  },
+  {
+    "revision": "40a00cf0c885719c6eacb087b5e7b25f",
+    "url": "/static/media/massey.40a00cf0.bin"
+  },
+  {
+    "revision": "9f58a3211c020336af689c4219152e02",
+    "url": "/static/media/massey_backup.9f58a321.bin"
+  },
+  {
+    "revision": "f2220ff29f82500b0256d7600f72fe57",
+    "url": "/static/media/otago.f2220ff2.bin"
+  },
+  {
+    "revision": "6443c58e16f06f0440b035fdd36ad684",
+    "url": "/static/media/people.6443c58e.png"
+  },
+  {
+    "revision": "2c66ddd09b0d0d278380c73bf0ebf199",
+    "url": "/static/media/uc.2c66ddd0.bin"
+  },
+  {
+    "revision": "40031763fa1aaf777900e8daf94aa97a",
+    "url": "/static/media/uoa.40031763.bin"
+  },
+  {
+    "revision": "9a1dffafe6f0bdba402c4a1253f725ba",
+    "url": "/static/media/victoria.9a1dffaf.bin"
+  },
+  {
+    "revision": "e6fe1d57159b2c62ba73fe7ca45db165",
+    "url": "/static/media/waikato.e6fe1d57.bin"
+  }
+]);
